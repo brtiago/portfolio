e7a3bdd8416ec2c8f2490a8e2b8389f30938777c
brtiago <tgribeiro@gmail.com>
2024-03-07 16:43:17 -0300

    feat: projects page

---
+++ src/app/pages/projects/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [
+    CardComponent
+  ],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}

fbda569645050d3a5a2ff05c896d32518b79d66d
brtiago <tgribeiro@gmail.com>
2024-03-06 21:00:34 -0300

    feat: hero component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatSidenavModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })

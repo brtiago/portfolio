7ac55fbd71f4397b31c3d233541f042c1d7115dd
brtiago <tgribeiro@gmail.com>
2024-03-11 16:46:45 -0300

    fix: ui button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UiButtonComponent } from './components/ui-button/ui-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, ContactComponent, FooterComponent, UiButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import { UiButtonComponent } from '../../components/ui-button/ui-button.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ClipboardModule, UiButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  value = "email@example.com";
 }

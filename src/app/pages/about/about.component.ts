7c9a61faf3932d826bb0b356ab6fc19b8741ec47
brtiago <tgribeiro@gmail.com>
2024-03-13 18:04:53 -0300

    feat: about component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  about = {
+    title: "About",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse  varius enim in eros elementum tristique. Duis cursus, mi quis viverra   ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+  };
 }

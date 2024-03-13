4f52c2316fce7608fee7a7a53ff362865cc4cf2d
brtiago <tgribeiro@gmail.com>
2024-03-13 18:06:24 -0300

    update: restyling components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { UiButtonComponent } from './components/ui-button/ui-button.component';
@@ -9,7 +10,14 @@ import { UiButtonComponent } from './components/ui-button/ui-button.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, ContactComponent, FooterComponent, UiButtonComponent],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent, 
+    HeroComponent, 
+    AboutComponent,
+    ContactComponent, 
+    FooterComponent, 
+    UiButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

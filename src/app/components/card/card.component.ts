72b36c3ac9747e27a97409d7954a228f5fb6d92a
brtiago <tgribeiro@gmail.com>
2024-03-25 18:56:00 -0300

    feat: card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,6 +3,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { ChipComponent } from '../../chip/chip.component';
 
 @Component({
   selector: 'app-card',
@@ -11,15 +12,25 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatChipsModule,
     MatDividerModule,
-    MatIconModule
+    MatIconModule,
+    ChipComponent,
   ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  @Input() tags = [
-    {name: 'Tagname'},
-    {name: 'Tagname'},
-    {name: 'Tagname'},
-  ];
+  @Input() card = {
+    title: 'Card Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, totam doloribus nulla provident ut quas. Soluta qui possimus odit animi quia et illo maiores, repellendus repudiandae modi! Atque, sapiente quidem?',
+    imgUrl:'../../../assets/imagens/pexels-luis-gomes-546819.jpg',
+    imgAlt:'Photo by Author',
+    tagList: [
+      {name: 'Tag 1'},
+      {name: 'Tag 2'},
+      {name: 'Tag 3'},
+      {name: 'Tag 4'},
+    ],
+    deployUrl: 'https://www.google.com/',
+    codeUrl:'https://github.com/',
+  }
 }
